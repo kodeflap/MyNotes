c4aabfb67a5e8869062ff132a4fddace06a98909
Pooja shaji <poojashaji321@gmail.com>
2022-10-12 23:33:34 +0530

    Added unit test for use case GetNote
    closes #4

---
+++ app/src/test/java/com/dlight/mynotes/feature_note/domain/use_cases/GetNotesTest.kt
@@ -0,0 +1,93 @@
+package com.dlight.mynotes.feature_note.domain.use_cases
+
+import com.dlight.mynotes.feature_note.domain.data.repository.FakeNoteRepository
+import com.dlight.mynotes.feature_note.domain.model.Note
+import com.dlight.mynotes.feature_note.domain.util.NoteOrder
+import com.dlight.mynotes.feature_note.domain.util.OrderType
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetNotesTest {
+
+    private lateinit var getNotes: GetNotes
+    private lateinit var fakeNoteRepository: FakeNoteRepository
+
+    @Before
+    fun setUp() {
+        fakeNoteRepository = FakeNoteRepository()
+        getNotes = GetNotes(fakeNoteRepository)
+
+        val noteToInsert = mutableListOf<Note>()
+        ('a'..'z').forEachIndexed { index, c ->
+            noteToInsert.add(
+                Note(
+                    title = c.toString(),
+                    content = c.toString(),
+                    timestamp = index.toLong(),
+                    color = index
+                )
+            )
+        }
+        noteToInsert.shuffle()
+        runBlocking {
+            noteToInsert.forEach { fakeNoteRepository.insertNote(it) }
+        }
+    }
+
+    @Test
+    fun `Order notes by title ascending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Title(OrderType.Ascending)).first()
+
+        for (i in 0..notes.size - 2) {
+            assertThat(notes[i].title).isLessThan(notes[i + 1].title)
+        }
+    }
+
+    @Test
+    fun `Order notes by title descending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Title(OrderType.Descending)).first()
+
+        for (i in 0..notes.size - 2) {
+            assertThat(notes[i].title).isGreaterThan(notes[i + 1].title)
+        }
+    }
+
+    @Test
+    fun `Order notes by date ascending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Date(OrderType.Ascending)).first()
+
+        for (i in 0..notes.size - 2) {
+            assertThat(notes[i].timestamp).isLessThan(notes[i + 1].timestamp)
+        }
+    }
+
+    @Test
+    fun `Order notes by date descending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Date(OrderType.Descending)).first()
+
+        for (i in 0..notes.size - 2) {
+            assertThat(notes[i].timestamp).isGreaterThan(notes[i + 1].timestamp)
+        }
+    }
+
+    @Test
+    fun `Order notes by color ascending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Color(OrderType.Ascending)).first()
+
+        for (i in 0..notes.size - 2) {
+            assertThat(notes[i].color).isLessThan(notes[i + 1].color)
+        }
+    }
+
+    @Test
+    fun `Order notes by color descending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Color(OrderType.Descending)).first()
+
+        for (i in 0..notes.size - 2) {
+            assertThat(notes[i].color).isGreaterThan(notes[i + 1].color)
+        }
+    }
+}
